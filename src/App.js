00f8e96c382d07a753bc43f19433149d6d5d17dd
Kapil Sharma <kapilsharmajmps@gmail.com>
2019-01-28 00:29:35 +0530

    CSS changed

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Header />
+        {/* <Header /> */}
         <div className='main-container'>
           <MainPage />
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     );
   }
